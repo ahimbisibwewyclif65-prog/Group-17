04e447905ac7f8241e9703188f8a1eec3eb8e459
ahimbisibwewyclif65-prog <ahimbisibwewyclif65@gmail.com>
2026-09-21 19:48:07 +0300

    Add files via upload
    java code

---
+++ Wyclif.java
@@ -0,0 +1,32 @@
+package Wyclif;
+import java.util.Scanner;
+public class Wyclif {
+
+	public static void main(String[] args) {
+	Scanner scanner =new Scanner(System.in);
+	String again="Yes";
+	while(again.equalsIgnoreCase("Yes")) {
+		System.out.print("Enter your age:");
+		int age = scanner.nextInt();
+		System.out.println("Are you a ugandan?(yes/no):");
+		String citizen =scanner.next();
+		System.out.println("Do you have a Second Class upper CGPA or better?(yes/no):");
+		String CGPA = scanner.next();
+		String ageCheck =(age>=18)?"ok":"Not ok";
+		String citizenCheck =citizen.equalsIgnoreCase("Yes")?"ok":"not ok";
+		String CGPAcheck =CGPA.equalsIgnoreCase("Yes")?"ok":"not ok";
+		System.out.println("Age requirement"+ ageCheck);
+		System.out.println("Citizen requirement"+citizenCheck);
+		System.out.println("CGPA requirement"+ CGPAcheck);
+	
+boolean eligible = ageCheck.equals("ok")&& citizenCheck.equals("ok")&& CGPAcheck.equals("ok");
+String result = eligible? "You qualify for the internship!":"You donot qualify for the internship.";
+System.out.println(""+result);
+System.out.println("check another applicant?(yes/no):");
+again = scanner.next();
+System.out.println("Thank you");
+scanner.close();
+	}
+	}
+
+}
